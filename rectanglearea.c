77b7e81071f5797344d573f32b14ce89c64d5493
Arnav Khare <your-real-github-email@example.com>
2026-05-25 13:52:23 +0530

    updated rectangle program

--- rectanglearea.c
+++ rectanglearea.c
@@ -2,11 +2,9 @@
 int main()
 {
     int length, breadth;
-
-    printf("enter the length/n");
+    printf("enter the length\n");
     scanf("%d", &length);
-
-    printf("enter the breadth/n");
+    printf("enter the breadth\n");
     scanf("%d", &breadth);
 
     printf("area of rectangle is %d", length * breadth);
