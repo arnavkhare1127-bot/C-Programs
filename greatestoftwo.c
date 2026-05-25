623359dacc7eb826a114c617720c8f8a06ba2a43
Arnav Khare <arnavkhare11.27@gmail.com>
2026-05-25 13:55:38 +0530

    Update greatestoftwo.c

--- greatestoftwo.c
+++ greatestoftwo.c
@@ -3,11 +3,9 @@
 int main() {
     int num1, num2;
 
-    // Ask user for input
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
 
-    // Compare the numbers
     if (num1 > num2) {
         printf("%d is the greatest.\n", num1);
     } else if (num2 > num1) {
