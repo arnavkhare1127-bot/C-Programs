2a25f4bb43e4a2c2ed0f9c19569c3cc5d7416653
Arnav Khare <your-real-github-email@example.com>
2026-05-25 13:51:22 +0530

    commit

---
+++ rectanglearea.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int length, breadth;
+
+    printf("enter the length/n");
+    scanf("%d", &length);
+
+    printf("enter the breadth/n");
+    scanf("%d", &breadth);
+
+    printf("area of rectangle is %d", length * breadth);
+
+    return 0;
+}
