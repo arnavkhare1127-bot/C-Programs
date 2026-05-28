ea9247c0daeb8f2ac5496367bf70e1fb5e0275f0
Arnav Khare <your-real-github-email@example.com>
2026-05-29 01:13:04 +0530

    commit

--- 14_naturalno.c
+++ 14_naturalno.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+/*Write a program to print first 'n' natural number using do-while loop*/
+#include <stdio.h>
 int main()
 {
     int n, i = 1;
     printf("enter the number\n");
     scanf("%d", &n);
-    do {
+    do
+    {
         printf("%d\n", i);
-    i++;
-    }
-    while(i<=n);
-    
+        i++;
+    } while (i <= n);
+
     return 0;
 }
