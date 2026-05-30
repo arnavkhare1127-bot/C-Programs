aa7742ab194f22d83d9b796dc7be4176a8b93ab5
Arnav Khare <arnavkhare11.27@gmail.com>
2026-05-30 17:16:55 +0530

    commit

--- 15_forloop.c
+++ 15_forloop.c
@@ -1,8 +1,10 @@
 /*Write a program to print first 'n' natural numbers using for-loop*/
 #include<stdio.h>
-int main (){
+int main ()
+{
     int n;
     scanf("%d", &n);
+    
     for (int i = 1; i<=n; i++)
     {
         printf("%d\n", i);
