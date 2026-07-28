b85899778a076b350ae2cd2906bda1ccf3b27964
Arnav Khare <arnavkhare11.27@gmail.com>
2026-07-29 03:47:44 +0530

    commit

---
+++ 36_pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i = 8;
+    int *j;
+    j = &i;
+    printf("address i= %u\n", &i);
+    printf("address i= %u\n", j);
+    printf("address j= %u\n", &j);
+    printf("value i= %d\n", i);
+    printf("value i= %d\n", *(&i));
+    printf("value i= %d\n", *j);
+    return 0;
+}
