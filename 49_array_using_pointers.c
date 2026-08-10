6308b2e8481b0c71e8f1c9c4b9969e6a24fcfba4
Arnav Khare <arnavkhare11.27@gmail.com>
2026-08-10 21:33:05 +0530

    commit

--- 49_array_using_pointers.c
+++ 49_array_using_pointers.c
@@ -4,14 +4,14 @@ int main()
     int marks[] = {12, 24, 36, 48};
     int *ptr = &marks[0];
 
-    // int* ptr=marks -->same as ---> int* ptr= &marks[0];
+    // int* ptr=marks; --> same as ---> int* ptr= &marks[0];
 
     for (int i = 0; i < 4; i++)
     {
 
         // printf("The address of marks %d is %u\n", i+1, &marks[i]);
 
-        printf("The address of marks %d is %u\n", i + 1, ptr);
+        printf("The value of marks %d is %u\n", i + 1, *ptr);
         ptr++;
     }
 
--- 51_2d_aray.c
+++ 51_2d_aray.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
+/*Create a 2-d array by taking input from the user. 
+Write a display function to print the content 
+of this 2-d array on the screen.*/
 
+#include <stdio.h>
 int main()
 {
     int arr[3][2];
