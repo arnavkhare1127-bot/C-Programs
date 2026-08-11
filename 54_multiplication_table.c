20b0e813b234a982c16109ea88169bab6c482e90
Arnav Khare <arnavkhare11.27@gmail.com>
2026-08-11 05:56:44 +0530

    commit

---
+++ 54_multiplication_table.c
@@ -0,0 +1,15 @@
+/*Write a program to create an array of 10 integers
+and store multiplication table of 5 in it.*/
+#include <stdio.h>
+int main()
+{
+    int arr[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = 5 * (i + 1);
+        printf("5 * %d = %d\n", (i + 1), 5 * (i + 1));
+    }
+
+    return 0;
+}
