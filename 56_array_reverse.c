453323e86a632d2e905303f3dfa06978381ffea4
Arnav Khare <arnavkhare11.27@gmail.com>
2026-08-12 17:34:20 +0530

    commit

--- 56_array_reverse.c
+++ 56_array_reverse.c
@@ -14,7 +14,7 @@ void reverse(int arr[], int n)
     int temp;
     for (int i = 0; i < n / 2; i++)
     {
-        temp=arr[i];
+        temp = arr[i];
         arr[i] = arr[n - i - 1];
         arr[n - i - 1] = temp;
     }
