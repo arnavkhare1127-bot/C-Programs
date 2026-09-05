4e54d2d23c4fbf9077056089302c398d5b2d37b1
Arnav Khare <arnavkhare11.27@gmail.com>
2026-09-05 22:48:21 +0530

    commit

---
+++ 76_structure.c
@@ -0,0 +1,37 @@
+/*Write a program to store the details of 3 employees from user defined data.*/
+#include <stdio.h>
+#include <string.h>
+struct employee
+{
+    int code;
+    char name[10];
+    float salary;
+};
+int main()
+{
+    struct employee e1, e2, e3;
+    printf("Enter the value of code\n");
+    scanf("%d", &e1.code);
+    printf("Enter the name\n");
+    scanf("%s", e1.name);
+    printf("Enter the salary\n");
+    scanf("%f", &e1.salary);
+    printf("%d %s %f\n", e1.code, e1.name, e1.salary);
+
+    printf("Enter the value of code\n");
+    scanf("%d", &e2.code);
+    printf("Enter the name\n");
+    scanf("%s", e2.name);
+    printf("Enter the salary\n");
+    scanf("%f", &e2.salary);
+    printf("%d %s %f\n", e2.code, e2.name, e2.salary);
+
+    printf("Enter the value of code\n");
+    scanf("%d", &e3.code);
+    printf("Enter the name\n");
+    scanf("%s", e3.name);
+    printf("Enter the salary\n");
+    scanf("%f", &e3.salary);
+    printf("%d %s %f\n", e3.code, e3.name, e3.salary);
+    return 0;
+}
